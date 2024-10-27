db8b7110e251fe7b24b45607f9b99e8af306ebfb
Kyrie-xuan <3220105557@qq.com>
2024-10-27 18:04:28 +0800

    Initial commit

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 import os
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS','localhost').split(',')
 from telnetlib import STATUS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
